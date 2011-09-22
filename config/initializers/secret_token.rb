63b808f5c3e45f89796e246d4b711ecb500a9309
Ashwin Arun Poothatta <ashwin.poothatta@sv.cmu.edu>
2011-09-21 17:12:47 -0700

    First commit for sblog

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sblog::Application.config.secret_token = '8fb23ead05fc88bed332c8a28ed35278cb72c0a3e7eed0eefe36873e8255a8b4a3715b8482f140a8ea17c836faef48d36f1febaf780fb22599659eaef22bf3b5'
